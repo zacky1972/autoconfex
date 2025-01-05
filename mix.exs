a3dd45491409f732388ead507f67dc957e64a552
Susumu Yamazaki <zacky1972@gmail.com>
2025-01-05 21:27:35 +0900

    build: set version to 0.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Autoconfex.MixProject do
   def project do
     [
       app: :autoconfex,
-      version: "0.1.0-dev",
+      version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
