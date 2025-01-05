a4f472a5fe8733e34a96de2568b438e67b485f29
Susumu Yamazaki <zacky1972@gmail.com>
2025-01-05 19:44:59 +0900

    remove hello

--- lib/autoconfex.ex
+++ lib/autoconfex.ex
@@ -2,17 +2,4 @@ defmodule Autoconfex do
   @moduledoc """
   Autoconfex: Auto-configuration of NIFs in C for Elixir.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Autoconfex.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/autoconfex_test.exs
+++ test/autoconfex_test.exs
@@ -1,8 +1,4 @@
 defmodule AutoconfexTest do
   use ExUnit.Case
   doctest Autoconfex
-
-  test "greets the world" do
-    assert Autoconfex.hello() == :world
-  end
 end
