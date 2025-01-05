b575d47e198dfe1ec3bfdfbdebfdf7f7b8ae2947
Susumu Yamazaki <zacky1972@gmail.com>
2025-01-05 19:21:32 +0900

    initial commit

---
+++ lib/autoconfex.ex
@@ -0,0 +1,18 @@
+defmodule Autoconfex do
+  @moduledoc """
+  Documentation for `Autoconfex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Autoconfex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/autoconfex_test.exs
@@ -0,0 +1,8 @@
+defmodule AutoconfexTest do
+  use ExUnit.Case
+  doctest Autoconfex
+
+  test "greets the world" do
+    assert Autoconfex.hello() == :world
+  end
+end
